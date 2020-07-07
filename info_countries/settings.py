3d4a1ff49b4bd12c07496100a0bedcca1d208507
Mehdi <sadour.mehdi@gmail.com>
2020-07-07 16:36:20 +0200

    Update settings.py

--- info_countries/settings.py
+++ info_countries/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f7^k-1kgpd^5h68jz!^wra)l19oxd922rk!yl40w#vw&g&@f#c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['info_countries.herokuapp.com']
+ALLOWED_HOSTS = ['info-all-countries.herokuapp.com']
 
 
 # Application definition
